943986e1ca12528ea8ade82a09a04443bea87a08
Sambasivarao <samba.pendela@gmail.com>
2021-11-25 11:42:33 +0530

    code

--- TypeScript/functions.js
+++ TypeScript/functions.js
@@ -1,37 +1,61 @@
-//function
-//particular "business" logic called as "function"
-//functions are used to "reuse" the business logic
+//functions
+//particular business logic called as "function"
+//functions are used to reuse the "business" logic
 //the function without name called as "anonymous" function
 //anonymous functions also called as "arrow" functions
-//we will represent arrow functions with "=>"
-//this concept introduced in "ES6"
+//arrow functions we will represent with "=>"
+//arrow functions introduced in "ES6"
 /*
-   //function definition
-   var/let/const variablename:datatype = (arg1:datatype,arg2:datatype,arg3:datatype,......argn:datatype):returntype=>{
-       business logic
-   }
+    1) function definition
 
-   //call the function
-   variablename(param1,param2,param3,......paramn);
-*/
-/*
-    let fun_one = (arg1:string,arg2:string,arg3:string):any=>{
-        console.log(arg1, arg2, arg3);
-    };
-    //fun_one();                                        //Expected 3 arguments, but got 0.
-    fun_one("Hello_1","Hello_2","Hello_3");             //Hello_1 Hello_2 Hello_3
-*/
-/*
-    let fun_one = (arg1:any,arg2:any,arg3:any):any=>{
-        console.log(arg1.msg, arg2.msg, arg3.msg);
+    2) function calling
+
+
+    function definition
+    *******************
+    var/let/const variablename:any = (arg1:datatype,arg2:datatype,arg3:datatype,.....argn:datatype):returntype=>{
+        business logic
     }
-    fun_one({"msg":"Hello_1"},{"msg":"Hello_2"},{"msg":"Hello_3"});             //Hello_1 Hello_2 Hello_3
+
+    function calling
+    ****************
+    variablename(param1,param2,param3,......paramn);
 */
-var networkCall = function (successCallBack, errorCallBack) {
-    console.log(successCallBack(), errorCallBack());
-};
-networkCall(function () {
-    return "Spring Boot Integration Soon....!";
-}, function () {
-    return "Error";
-});
+/*
+    let fun_one = ():string=>{
+        return "welcome to arrow functions";
+    }
+    console.log( fun_one() );                   //welcome to arrow functions
+*/
+/*
+    let fun_one = ():void =>console.log("Hello");
+    fun_one();                  //Hello
+*/
+/*
+    let fun_one = (arg1:string,arg2:string,arg3:string):void=>console.log(arg1,arg2,arg3);
+    //fun_one();                                    //Expected 3 arguments, but got 0.
+    fun_one("Hello_1","Hello_2","Hello_3");         //Hello_1 Hello_2 Hello_3
+*/
+/*
+    let networkCall = (arg1:any,arg2:any):void=>{
+        console.log( arg1(), arg2() );
+    }
+    networkCall( ():string=>{ return "Hello_1" }, ():string=>{ return "Hello_2" } );            //Hello_1 Hello_2
+*/
+//typeof
+console.log(typeof "Angular13"); //string
+console.log(toString.call("Angular13")); //[object String]
+console.log(typeof 100); //number
+console.log(toString.call(100)); //[object Number]
+console.log(typeof true); //boolean
+console.log(toString.call(false)); //[object Boolean]
+console.log(typeof undefined); //undefined
+console.log(toString.call(undefined)); //[object Undefined]
+console.log(typeof null); //object
+console.log(toString.call(null)); //[object Null]
+console.log(typeof []); //object
+console.log(toString.call([])); //[object Array]
+console.log(typeof {}); //object
+console.log(toString.call({})); //[object Object]
+console.log(typeof function fun_one() { }); //function
+console.log(toString.call(function fun_one() { })); //
